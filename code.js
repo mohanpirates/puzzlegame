33004b64cef91fe88b53bccbaeaf5a11c037f57b
mohanpirates <mohanmark42@gmail.com>
2017-09-07 18:59:48 +0530

    second commit

---
+++ code.js
@@ -0,0 +1,22 @@
+//if click on start/reset
+//if we are playing
+  //reload page
+//if we are not playing
+   //set score to 0
+//show countdown box
+//reduce time by 1sec in loops
+  //timeleft
+    //yes->continue
+     //no->game over
+//change button to reset
+ //generate new code Q&A
+
+//if we click on answer box
+  //if we are playing
+     //correct?
+       //yes
+               //increase score
+              //show correct box for 1sec
+              //generate new Q&A
+          //no
+           //show try again box for 1sec
